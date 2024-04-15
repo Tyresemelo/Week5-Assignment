54f96f8f8b16b6598a3fbe2f56a457598c608059
KalvinBasha <kalvi481@gmail.com>
2024-04-15 15:31:11 +0100

    first branch added kal

--- server/script.js
+++ server/script.js
@@ -1,2 +1,34 @@
-console.log("hello");
-console.log("Hello World");
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(express.json());
+app.use(cors()); // this allows our client to communicate with the server wiithout being blocked
+
+// connect to a database.db file (normally this would be "connect to the databse that is hosted elsewhere")
+import Database from "better-sqlite3";
+const db = new Database("database.db"); // go and get the existing db file
+
+app.get("/", function (request, response) {
+  response.json("You are looking at my root route.");
+});
+
+app.get("/reviews", function (request, response) {
+  // here we use .all instead of .run because we aren't INSERTing, but selecting. So we want ALL the results back
+  const reviews = db.prepare("SELECT * FROM reviews").all();
+  response.json(reviews);
+});
+
+app.post("/message", function (request, response) {
+  const newMessage = request.body;
+  // this console log will appear in the terminal because that is where the server is running
+  console.log(newMessage);
+
+  // here is the response. At the moment we are just sending back what the client sent with their own request
+  // soon we will do stuff with that information, like adding it to a database
+  response.json(newMessage);
+});
+
+app.listen(8080, function () {
+  console.log("App is running on port 8080");
+});
