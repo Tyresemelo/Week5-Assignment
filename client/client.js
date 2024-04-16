cbd99612cf0c3bdbab4f1e5442418f3608b9eb67
Tyresemelo <Tyrese.melo@icloud.com>
2024-04-16 13:47:25 +0100

    feth data correction

--- client/client.js
+++ client/client.js
@@ -16,20 +16,13 @@ function handleSubmit(event) {
         }
     });
 
-    async function getReview() {
-        const response = await fetch("http://localhost8080/review");
-        const guestbook = await response.json();
-        console.log(guestbook);
-    }
-
-    
 async function getReview() {
-const response = await fetch("http://localhost8080/guestbook");
-const guestbook = await response.json();
+const response = await fetch("http://localhost8080/review");
+const review = await response.json();
 console.log(review);
 
   // put the games onto the page
-games.forEach(function (review) {
+review.forEach(function (review) {
     // DOM manipulation to put the games onto the html
     const h2 = document.createElement("h2");
     const p = document.createElement("p");
